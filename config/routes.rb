84d2fb19daa6755f53164033e48234c708778916
Fernando Gonzalez <fgonz009@ucr.edu>
2017-08-09 00:23:19 -0700

    Added a Sign Up page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  #Add a Post resouce example
+  #post 'users', to: 'users#create'
+
 end
